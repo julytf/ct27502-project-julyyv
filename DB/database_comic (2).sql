b432c0bca300eb4092ce6fecf2aa5253f9693d60
julyyv <chanvucktv@gmail.com>
2023-04-10 10:08:47 +0700

    Update database_comic (2).sql

--- DB/database_comic (2).sql
+++ DB/database_comic (2).sql
@@ -16,11 +16,27 @@
 
 
 -- Dumping database structure for comics
-CREATE DATABASE IF NOT EXISTS `comics` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `comics`;
+CREATE DATABASE IF NOT EXISTS `comictv` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `comictv`;
+
+-- Dumping structure for table comics.comics
+CREATE TABLE IF NOT EXISTS `comics` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `description` text,
+  `cover_image` text,
+  `status` tinyint(1) DEFAULT NULL,
+  `author` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- Dumping data for table comics.comics: ~2 rows (approximately)
+INSERT INTO `comics` (`id`, `name`, `description`, `cover_image`, `status`, `author`) VALUES
+	(1, 'one piece', NULL, 'abc.png', NULL, NULL),
+	(2, 'optimis', 'hay chon gia dung', 'https://cdn.vox-cdn.com/thumbor/oylb3MGb1jQk3LOP2N3c4eaW-dg=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/24390847/last_knight_optimus.jpeg', 'dang cap nhat', NULL);
 
 -- Dumping structure for table comics.chapter
-CREATE TABLE IF NOT EXISTS `chapter` (
+CREATE TABLE IF NOT EXISTS `chapters` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `index_chapter` float NOT NULL,
   `name` varchar(100) DEFAULT NULL,
@@ -31,29 +47,22 @@ CREATE TABLE IF NOT EXISTS `chapter` (
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table comics.chapter: ~3 rows (approximately)
-INSERT INTO `chapter` (`id`, `index_chapter`, `name`, `comic_id`) VALUES
+INSERT INTO `chapters` (`id`, `index_chapter`, `name`, `comic_id`) VALUES
 	(1, 1, 'bat dau', 1),
 	(2, 2, 'giua tran', 1),
 	(3, 3, 'ket thuc', 1);
 
--- Dumping structure for table comics.comics
-CREATE TABLE IF NOT EXISTS `comics` (
+-- Dumping structure for table comics.genre
+CREATE TABLE IF NOT EXISTS `genres` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
-  `description` text,
-  `image` text NOT NULL,
-  `cover_image` text,
-  `status` tinyint(1) DEFAULT NULL,
-  `others_name` varchar(100) DEFAULT NULL,
-  `country` varchar(100) DEFAULT NULL,
-  `release_date` date DEFAULT NULL,
+  `description` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
--- Dumping data for table comics.comics: ~2 rows (approximately)
-INSERT INTO `comics` (`id`, `name`, `description`, `image`, `cover_image`, `status`, `others_name`, `country`, `release_date`) VALUES
-	(1, 'one piece', NULL, 'abc.png', NULL, NULL, NULL, NULL, NULL),
-	(2, 'optimis', 'hay chon gia dung', 'https://cdn.vox-cdn.com/thumbor/oylb3MGb1jQk3LOP2N3c4eaW-dg=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/24390847/last_knight_optimus.jpeg', NULL, 1, NULL, NULL, NULL);
+-- Dumping data for table comics.genre: ~0 rows (approximately)
+INSERT INTO `genres` (`id`, `name`, `description`) VALUES
+	(1, 'hai tac', 'di tim kho bau');
 
 -- Dumping structure for table comics.comic_genre
 CREATE TABLE IF NOT EXISTS `comic_genre` (
@@ -62,38 +71,26 @@ CREATE TABLE IF NOT EXISTS `comic_genre` (
   PRIMARY KEY (`comic_id`,`genre_id`),
   KEY `genre_id` (`genre_id`),
   CONSTRAINT `comic_genre_ibfk_1` FOREIGN KEY (`comic_id`) REFERENCES `comics` (`id`),
-  CONSTRAINT `comic_genre_ibfk_2` FOREIGN KEY (`genre_id`) REFERENCES `genre` (`id`)
+  CONSTRAINT `comic_genre_ibfk_2` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table comics.comic_genre: ~1 rows (approximately)
 INSERT INTO `comic_genre` (`comic_id`, `genre_id`) VALUES
 	(1, 1);
 
--- Dumping structure for table comics.genre
-CREATE TABLE IF NOT EXISTS `genre` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `description` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- Dumping data for table comics.genre: ~0 rows (approximately)
-INSERT INTO `genre` (`id`, `name`, `description`) VALUES
-	(1, 'hai tac', 'di tim kho bau');
-
 -- Dumping structure for table comics.image
-CREATE TABLE IF NOT EXISTS `image` (
+CREATE TABLE IF NOT EXISTS `images` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `file` text NOT NULL,
   `index_image` float NOT NULL,
   `chapter_id` bigint NOT NULL,
   PRIMARY KEY (`id`),
   KEY `chapter_id` (`chapter_id`),
-  CONSTRAINT `image_ibfk_1` FOREIGN KEY (`chapter_id`) REFERENCES `chapter` (`id`)
+  CONSTRAINT `image_ibfk_1` FOREIGN KEY (`chapter_id`) REFERENCES `chapters` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table comics.image: ~9 rows (approximately)
-INSERT INTO `image` (`id`, `file`, `index_image`, `chapter_id`) VALUES
+INSERT INTO `images` (`id`, `file`, `index_image`, `chapter_id`) VALUES
 	(1, 'http://pic0.iqiyipic.com/image/20211206/f2/2e/a_100421840_m_601_en_m1_1013_638.jpg', 1, 1),
 	(2, 'http://pic0.iqiyipic.com/image/20211206/f2/2e/a_100421840_m_601_en_m1_1013_638.jpg', 2, 1),
 	(3, 'http://pic0.iqiyipic.com/image/20211206/f2/2e/a_100421840_m_601_en_m1_1013_638.jpg', 3, 1),
